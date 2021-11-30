44ec1523d7fed6969becf4e6ebcde9c3f84dfe0b
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-11-30 22:57:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+#include <algorithm>
+using namespace std;
 
 int main(){
+  char a=0;
+  cin>>a;
+  if (funzione(a)==false){
+    cout<< "errore"" << endl;
+    }
+  else{
+    int ascii=a;
+    if (97<= a){
+      ascii= toupper(a);
+      cout<< char(ascii)<< endl;
+    }else if (65<= ascii <=90){
+      cout<< char (tolower(a)) << endl;
+    }
+  }
   return 0;
 }
